00ef8e23e5bcfa9c5f34703f49750bdd0e1240f1
onsen_manjuuu <62044948+onsen-manjuuu@users.noreply.github.com>
2023-07-14 23:31:46 +0900

    refine blog page

--- pages/blog.js
+++ pages/blog.js
@@ -1,6 +1,20 @@
+import { getSortedPostsData } from '../lib/posts'
+import Layout from '../components/layout';
 
+export async function getStaticProps() {
+    const allPostsData = getSortedPostsData();
+    return {
+        props : {
+            allPostsData,
+        }
+    };
+}
 
 
 export default function blogList() {
-
+    return (
+    <Layout>
+        <h1>blog</h1>
+    </Layout>
+    )
 }
