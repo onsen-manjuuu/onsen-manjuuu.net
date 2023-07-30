d46ee1c58babcd6335ef6c239c8abf1b32b868ab
onsen_manjuuu <62044948+onsen-manjuuu@users.noreply.github.com>
2023-07-30 22:28:31 +0900

    add images

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -3,6 +3,7 @@ import { getAllPostIds, getPostData } from '../../lib/posts';
 import Head from 'next/head'
 import Date from '../../components/date';
 import styles from '../../styles/indivBlogs.module.css'
+import Image from 'next/image';
 
 export default function Post({postData}) {
     return (
